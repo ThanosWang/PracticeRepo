c645ae617f1a84ca3202082253cea1aae0bf1fac
ZijunWang <101375426+ThanosWang@users.noreply.github.com>
2022-08-12 08:50:19 -0500

    Update metadata_upload_test.py

--- metadata_upload_test.py
+++ metadata_upload_test.py
@@ -10,9 +10,7 @@ else:
     File_list = All_added_files.split(',')
     for file in File_list:
         newmetadata = file.split('/')[-1]
-        path = os.getcwd(file)
-        print(path)
-        os.chdir(path)
+        os.chdir('Metadata')
         filelist = os.listdir()
 
         # List of keys in metadata file
